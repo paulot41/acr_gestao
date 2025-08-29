f82134f33ed8b66ada720ce854eeb1b5922b6c12
Paulo Teixeira <paulo.t.41@gmail.com>
2025-08-30 00:41:58 +0100

    multitenant

--- acr_gestao/multitenant.py
+++ acr_gestao/multitenant.py
@@ -1,5 +1,7 @@
-from django.utils.deprecation import MiddlewareMixin
 from threading import local
+from django.utils.deprecation import MiddlewareMixin
+from django.db import connection
+from django.db.utils import ProgrammingError, OperationalError
 from core.models import Organization
 
 _local = local()
@@ -10,9 +12,17 @@ def get_current_org():
 class OrganizationMiddleware(MiddlewareMixin):
     def process_request(self, request):
         host = request.get_host().split(":")[0].lower()
+        org = None
         try:
-            org = Organization.objects.get(domain=host)
-        except Organization.DoesNotExist:
+            # Só tenta se as tabelas já existem
+            if connection.introspection.table_names():
+                try:
+                    org = Organization.objects.get(domain=host)
+                except Organization.DoesNotExist:
+                    org = None
+        except (ProgrammingError, OperationalError):
+            # BD ainda não migrada ou indisponível
             org = None
+
         _local.org = org
         request.organization = org
